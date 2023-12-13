fd0d24be46dd0a56db3c778f2bc784e5aa259482
delwin23 <delwinkuriakose23@gmail.com>
2023-12-13 14:07:56 +0530

    ab

---
+++ ADS/addmatrix.c
@@ -0,0 +1,28 @@
+//sum of two matrix
+#include<stdio.h>
+int main()
+{
+	int r,c,a[10][10],b[10][10],sum[10][10],i,j;
+	printf("eneter the row of matrix 1\n");
+	scanf("%d",&r);
+	printf("enter the column of matrix1\n");
+	scanf("%d",&c);
+
+printf("enter the elements into the matrix\n");
+for(i=0;i<=r;i++)
+	for(j=0;j<=c;j++)
+		{
+		scanf("%d",&a[i][j]);
+		}
+		}
+	
+	for(i=0;i<=r;i++)
+	for(j=0;j<=c;j++)
+	{
+	printf("%d",a[i][j]);
+	}
+	printf("\n\n");
+	}
+	}
+
+	
